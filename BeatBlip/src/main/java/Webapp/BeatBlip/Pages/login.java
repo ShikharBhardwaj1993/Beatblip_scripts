c98404fb39dd2a29a9bf39f83b9e8f46d60473ef
Shikhar Bhardwaj <shikharbhardwaj1993@gmail.com>
2021-11-30 23:16:20 +0530

    first beatblip commit

---
+++ BeatBlip/src/main/java/Webapp/BeatBlip/Pages/login.java
@@ -0,0 +1,36 @@
+package Webapp.BeatBlip.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class login {
+
+	
+	@FindBy(xpath="//*[@formcontrolname='userName']")
+	WebElement ID ;
+		
+	@FindBy(xpath="//*[@formcontrolname='password']")
+	WebElement Password ;
+	
+	@FindBy(xpath="//button[@type='submit']")
+	WebElement loginclick ;
+	
+	public WebDriver Driver ;
+	
+	public login(WebDriver driver){
+		
+		this.Driver=driver;
+		PageFactory.initElements(Driver,this) ;
+		
+	}
+	
+	public void loginBB(){
+		
+		ID.sendKeys("shikhar.bhardwaj@agreeya.com");
+		Password.sendKeys("test@123");
+		loginclick.click();
+	}	
+	
+}
